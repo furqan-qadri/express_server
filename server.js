c714bf42b1ace18180978f01762b034c9e60da9d
“Furqan <furqaanqadri@gmail.com>
2025-01-02 19:04:11 +0000

    add morgan and nodemon

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+
 const app = express();
 const PORT = 4000;
+
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello from express");
 });
 
+app.get("/furqan", (req, res) => {
+  res.send("Hello from furqan and reeb");
+});
+
 app.listen(PORT, () => {
-  console.log(`the app is listening on port ${PORT}`);
+  console.log(`the app is listening on port no: ${PORT}`);
 });
